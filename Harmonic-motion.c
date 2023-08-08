dfa8f469dadaf380bd800e1a9002373ecde0b39d
Nima Shirani <88476356+n101ima@users.noreply.github.com>
2023-08-08 22:40:51 +0330

    Update Harmonic-motion.c

--- Harmonic-motion.c
+++ Harmonic-motion.c
@@ -1,12 +1,12 @@
 #include "udf.h"
-#define Freq 3.95662518 // Frequency in hertz
-#define angular_freq 2.0M_PIFreq // Angular frequency in radians per second
-#define tetmax 90.0*M_PI/180 // Maximum angular displacement in radians
+#define Freq 3.95662518			
+#define angular_freq 2.0*M_PI*Freq
+#define tetmax 90.0*M_PI/180
 DEFINE_PROFILE(velocity_profile,t,i)
 {
-real time = CURRENT_TIME; // Get current time
-face_t f; // Declare face variable
-begin_f_loop(f,t) // Loop over faces in face zone t
+real time = CURRENT_TIME;
+face_t f;
+begin_f_loop(f,t)
 {
 	F_PROFILE(f,t,i) = tetmax*angular_freq*cos(angular_freq*time);
 }
